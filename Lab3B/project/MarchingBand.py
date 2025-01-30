79b0a07bf4d9451ed176cafbad4c6fb41b6ec3b4
jambrose2 <julianambrose2@gmail.com>
2025-01-30 16:12:25 -0500

    maybe working code

--- Lab3B/project/MarchingBand.py
+++ Lab3B/project/MarchingBand.py
@@ -6,7 +6,9 @@ import threading
 import FluteSensors
 
 motor = Motor("A")
-US_SENSOR = EV3UltrasonicSensor(...)
+US_SENSOR_STOP = EV3UltrasonicSensor(...)
+US_SENSOR_DRUM = EV3UltrasonicSensor(...)
+
 wait_ready_sensors()
 
 stopEvent = threading.Event()
@@ -15,14 +17,30 @@ threshold = 10 # How close to trigger the STOP mechanism
 
 def emergencyStop(t):
     while not stopEvent.is_set():
-        distance = US_SENSOR.get_distance()
+        distance = US_SENSOR.get_cm()
         if distance < threshold:
             stopEvent.set()
         time.sleep(.2)
 
 def drums():
-    motor.set_dps(dps=1000)
-    motor.
+    try:
+        while not stopEvent.is_set():
+            motor.set_dps(dps=1000)
+            motor.set_position_relative(90)
+            time.sleep(0.5)
+            motor.set_position_relative(-90)
+            time.sleep(0.5)
+            motor.set_position_relative(90)
+            time.sleep(0.3)
+            motor.set_position_relative(-90)
+            time.sleep(0.3)
+            motor.set_position_relative(90)
+            time.sleep(0.8)
+            motor.set_position_relative(-90)
+        motor.set_power(0)
+    except BaseException:
+        motor.set_power(0)
+        exit()
 
 
 if __name__ == '__main__':
@@ -31,5 +49,22 @@ if __name__ == '__main__':
     fluteThread.start()
     emergencyThread = threading.Thread(target=emergencyStop, daemon=True)
     emergencyThread.start()
+    FluteSensors.initializeFlute()
+    drumOn = False
+    while not stopEvent.is_set():
+        d = US_SENSOR_DRUM.get_cm()
+        if d < threshold:
+            drumOn = True
+            drumThread = threading.Thread(target=drums, daemon=True)
+            drumThread.start()
+
+    if drumThread:
+        drumThread.join()
+
+    fluteThread.join()
+
+
+
+
 
 
