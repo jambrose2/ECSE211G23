7f897cad1f5f39b526b3f9c679a906218d648054
jambrose2 <julianambrose2@gmail.com>
2025-02-06 11:03:25 -0500

    final code

--- Lab3B/project/FluteSensors.py
+++ Lab3B/project/FluteSensors.py
@@ -1,18 +1,26 @@
 from utils.brick import wait_ready_sensors, TouchSensor, EV3UltrasonicSensor
 from utils import sound
+import time
 
+
+# Declare notes with distinct pitches
 note1 = sound.Sound(duration=0.3, pitch="F#4", volume=80)
 note2 = sound.Sound(duration=0.3, pitch="Bb3", volume=80)
 note3 = sound.Sound(duration=0.3, pitch="Eb5", volume=80)
 note4 = sound.Sound(duration=0.3, pitch="D#4", volume=80)
 
 
+# Declare sensors
 touch1 = TouchSensor(1)
 touch2 = TouchSensor(2)
 US = EV3UltrasonicSensor(3)
 
-cutoff = 10
 
+# Threshold to trigger the ultrasonic sensor
+cutoff = 5
+
+
+# Function that takes int i as argument and plays the associated note
 def play_sound(i):
     if (i == 1):
         note1.play()
@@ -29,22 +37,36 @@ def play_sound(i):
     return
 
 
-def initializeFlute():
+
+# Function to wait for user to play the flute, takes stopEvent as argument
+def initializeFlute(stopEvent):
     try:
-        while True:
+        # Loop infinitely until emergency stop
+        while not stopEvent.is_set():
+            # Trigger notes according to sensor input
             if touch1.is_pressed() and touch2.is_pressed():
                 play_sound(4)
             elif touch1.is_pressed():
                 play_sound(1)
             elif touch2.is_pressed():
                 play_sound(2)
-            elif US.get_cm() < cutoff:
-                play_sound(3)
+            # Check multiple times for ultrasonic distance to not play notes on 
+            # faulty readings
+            if US.get_cm() < cutoff:
+                time.sleep(0.01)
+                if US.get_cm() < cutoff:
+                    time.sleep(0.01)
+                    if US.get_cm() < cutoff:
+                        time.sleep(0.1)
+                        play_sound(3)
+            # Brief sleep to not overload system
             time.sleep(0.05)
             
+    # Catch and print exceptions
     except Exception as e:
         print("Error: " + str(e))
-        sound.beep()
 
+
+# Tester
 if __name__ == '__main__':
     initializeFlute()
--- Lab3B/project/MarchingBand.py
+++ Lab3B/project/MarchingBand.py
@@ -5,23 +5,32 @@ import time
 import threading
 import FluteSensors
 
-motor = Motor(A)
+
+# Initialize motors and color sensor
+motor = Motor('A')
 cSensor = EV3ColorSensor(3)
 
-
+# Allow sensors to set up
 wait_ready_sensors()
 
+
+# Declare stop event
 stopEvent = threading.Event()
 
 
-
+# Classification algorithm for color sensor values
 def colorIdentifier(rgb):
+    # Return 0 if missing any values
     if rgb[0] == None or rgb[1] == None or rgb[2] == None:
         return 0
+    # Sum RGB values
     s = rgb[0] + rgb[1] + rgb[2]
-    if s < 100:
+    # Return 0 if the value is not a strong reading
+    if s < 85:
         return 0
+    # Normalize RGB values
     newRGB = [rgb[0]/s, rgb[1]/s, rgb[2]/s]
+    # Identify the color based on testing clusters
     if newRGB[0] > 0.6:
         if newRGB[1] < 0.4:
             if newRGB[2] < 0.3:
@@ -30,69 +39,102 @@ def colorIdentifier(rgb):
         if newRGB[0] < 0.25:
             if newRGB[2] < 0.2:
                 return 2
-    # elif newRGB[2] > 0.375:
-     #   if newRGB[0] < 0.225:
-        #    if newRGB[1] < 0.4 and newRGB[1] > 0.1:
-             #   return 3
+    # Return 0 if doesn't strongly match a color
     else:
         print("Unable to identify")
         return 0
 
+
+# Function to read color sensor
 def colorLoop():
+    # Set variable for drum status
     drumOn = False
+    # Loop infinitely until emergency stop
     while not stopEvent.is_set():
-        print(drumOn)
+        # Won't run without some print here, asked multiple TA's, tried to sleep 
+        # too. Both TA's said it was fine to leave 
+        print('')
+        # Identify current color status
         color = cSensor.get_rgb()
         i = colorIdentifier(color)
 
+        # If color is red, turn drum off, stop flute and exit
         if i == 1:
-            print("Red")
+            # print("Red")
+            # Check if drums on
             if drumOn:
+                motor.set_power(0)
                 drumOn = False
                 stopEvent.set()
-                motor.set_power(0)
-                stopEvent.clear()
+                time.sleep(0.5)
+                drumThread.join()
+                fluteThread.join()
                 exit()
-
+        
+        # If color green, start drums
         elif i == 2:
-            print("Green")
+            # print("Green")
+            # Check if drums already on
             if not drumOn:
                 stopEvent.clear()
+                # Start a thread for the drums
                 drumThread = threading.Thread(target=drums)
                 drumThread.start()
                 drumOn = True
-        # elif colorIdentifier(color) == 3:
-        #     print("Blue")
+        # Continue if not green or red
         else:
             continue
 
+
+# Function to control drum movement
 def drums():
     try:
+        # Loop infinitely until emergency stop
         while not stopEvent.is_set():
+            
+            # Rotate motor up and down, pausing in between to make a melody
             motor.set_dps(dps=1000)
-            motor.set_position_relative(90)
+            motor.set_position_relative(100)
             time.sleep(1)
-            motor.set_position_relative(-90)
-            time.sleep(1.5)
-            motor.set_position_relative(90)
+            # Check emergency stop for a quicker exit
+            if (stopEvent.is_set()):
+                motor.set_power(0)
+                break
+            motor.set_position_relative(-100)
+            time.sleep(1.2)
+            if (stopEvent.is_set()):
+                motor.set_power(0)
+                break
+            motor.set_position_relative(100)
             time.sleep(1.3)
-            motor.set_position_relative(-90)
-            time.sleep(1.3)
-            motor.set_position_relative(90)
-            time.sleep(1.8)
-            motor.set_position_relative(-90)
-            time.sleep(1.8)
+            if (stopEvent.is_set()):
+                motor.set_power(0)
+                break
+            motor.set_position_relative(-100)
+            time.sleep(0.8)
+            if (stopEvent.is_set()):
+                motor.set_power(0)
+                break
+            motor.set_position_relative(100)
+            time.sleep(1.6)
+            if (stopEvent.is_set()):
+                motor.set_power(0)
+                break
+            motor.set_position_relative(-100)
+            time.sleep(1.1)
         motor.set_power(0)
     except Exception as e:
         print("Drums error: ", e)
+    # Turn off motor
     finally:
         motor.set_power(0)
 
 
 if __name__ == '__main__':
-    fluteThread = threading.Thread(target=FluteSensors.initializeFlute, daemon=True)
-    # fluteThread.daemon = True
+    # Start thread for flute and run
+    fluteThread = threading.Thread(target=FluteSensors.initializeFlute, args=(stopEvent,), daemon=True)
     fluteThread.start()
+    # Start thread for color sensor and run
     ColorSensorLoop = threading.Thread(target=colorLoop, daemon=True)
     ColorSensorLoop.start()
     
